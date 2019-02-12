69cb468d68e1d8d2802cd303b36e9340bb689a99
Tuomas Ukkola <tuomasukk@gmail.com>
2019-02-12 19:34:27 +0200

    add heart to scene

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,45 +1,63 @@
 'use strict';
 
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroAmbientLight,
+  ViroSpotLight,
+  Viro3DObject,
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
+  state = {
+    text: 'Initializing AR...',
+    ready: false,
+  };
 
-  constructor() {
-    super();
-
-    // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
-    };
-
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
-
-  render() {
-    return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      </ViroARScene>
-    );
-  }
-
-  _onInitialized(state, reason) {
+  onInitialized = (state, reason) => {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: '',
+        ready: true,
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
+  };
+
+  render() {
+    const {ready} = this.state;
+    return (
+      <ViroARScene onTrackingUpdated={this.onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroAmbientLight color={'#aaaaaa'} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color='#ffffff'
+          castsShadow={true}
+        />
+        {ready && (
+          <Viro3DObject
+            source={require('../res/heart/love_heart.obj')}
+            position={[0, 0, -1]}
+            scale={[0.2, 0.2, 0.2]}
+            type='OBJ'
+          />
+        )}
+      </ViroARScene>
+    );
   }
 }
 
@@ -49,7 +67,7 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
 
